c1f0554692c2d2ca5dad552852d606fad5cc4bb5
“shamnad” <hamzashamnad123@gmail.com>
2023-12-21 21:46:13 +0530

    'sv'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 
+
+import './App.css'
+
+import  Header from './components/Header';
+
+import Banner from './components/Header';
+
 function App() {
+
+  const data='IT IS THE DATA INSERTED'
+
+  const jaba='SAMPLE DATA FOR TESTING'
+
   return (
-   <h1>Hello mars </h1>
+    <div>
+
+      <h1 style={{backgroundColor:'red'}}>Hello mars </h1>
+
+      <br/>
+
+      <p style={{color:'blue'}}>{data}</p>
+
+      <h2 className='mac'>hooi saitama</h2>
+
+      <h2 className='stac'>hooi gokuuuu</h2>
+
+      {/* <Shami/>
+        <Shami/>
+        <Shami/>
+        <Eldho/>
+        <Eldho/> 
+      */}
+
+      <Header datta={data} />
+      <Banner jabba={jaba} />
+
+   </div>
   );
 }
 
 export default App;
+
+
+// function Shami(){ //need use captial letter first letter
+//   return(
+//     <h5 className='mac'>batman</h5>
+//   )
+// }
+
+// function Eldho(){
+//   return(
+//   <h1 className='stac'>superman</h1>
+//   )
+// }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+import {Component} from 'react' //mention first in class component
+
+function Header(props){
+
+    return(<h1>helloo I'm shamnad '{props.datta}' by shamnad</h1>)
+}
+
+export default Header
+
+
+// class commponent
+
+class Banner extends Component{
+    render(){
+            return(<h1> hello I'm {this.props.jabba}</h1>)
+        }
+
+}
+
+// export default Banner
