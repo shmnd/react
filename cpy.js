17452d2885b515fa136068518781fc3df1736485
“shamnad” <hamzashamnad123@gmail.com>
2023-12-22 15:09:10 +0530

    arrow function

---
+++ cpy.js
@@ -0,0 +1,50 @@
+// Normal function
+
+        // function message(){
+        //     console.log('It is a beautifull morning');
+        // }
+
+        // message();
+
+
+// Anonymus function 
+
+    // it does'nt have name 
+
+    //    let fun = function(){
+    //         console.log('A function without function name its called anonymus function ,It is example for anonymus function');
+    //     }
+
+    //     fun();
+
+
+
+// Arrow function
+
+        // let abc = () => console.log('Hello world');
+
+
+        // abc();
+
+
+
+// ARROW FUNCTIOIN USING ARGUEMENTS AND PARAMETERS
+
+        // let sum = (a,c) => console.log(a+c)
+
+        // sum(5,15);
+
+
+
+    
+// ARROW FUNCTION USING  IF CONDITIONS
+
+        // let negative = (a) => {if (a>0){
+        //     console.log('positive')
+        // } else {
+        //     console.log('negative')
+        // }
+        // }
+
+        // //  negative(10);
+        // negative(-10);
