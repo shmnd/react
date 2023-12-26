c1291d9a19bd393fbb6917c7dd9b5c60844a30a2
“shamnad” <hamzashamnad123@gmail.com>
2023-12-26 18:32:16 +0530

    update

--- src/Useeffect.jsx
+++ src/Useeffect.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Useeffect() {
+    const [count,setCount]=useState(0)
   return (
     <div>
-       <h1>Hi, I'm component</h1>
+       <button onClick={()=>setCount(count+1)}>Increament</button> 
+       <h1>Hi, I'm component: {count}</h1>
     </div>
   )
 }
